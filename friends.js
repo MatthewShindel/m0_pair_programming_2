6a175181dca147d084eba403d93a74c0f56cb1a8
MatthewShindel <matthew@shindel.com>
2023-09-20 22:44:18 -0400

    add places array

--- friends.js
+++ friends.js
@@ -2,3 +2,5 @@ var arrays = ["Alex", "Greg","Daisy","Peter","Joe"];
 console.log(arrays[2])
 console.log(arrays[4])
 console.log(arrays[0])
+var places = ["New York", "India", "Brazil", "Canada","Stamford"];
+console.log(places.pop())
